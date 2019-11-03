e2998c2acd9f74ce09d45549d7fe27cd02324115
Liu <racliu@8c85904c43d0.ant.amazon.com>
2019-11-03 21:37:19 +0800

    kafka dev

--- kafka/kafka_dev.sh
+++ kafka/kafka_dev.sh
@@ -1,33 +1,42 @@
-
-#JDK
-#https://docs.aws.amazon.com/corretto/latest/corretto-8-ug/downloads-list.html
-yum install java-1.8.0
-
-
-
-mkdir -p /installer
-cd /installer
-wget http://apache.mirror.amaze.com.au/zookeeper/zookeeper-3.5.6/apache-zookeeper-3.5.6.tar.gz
-gzip -d apache-zookeeper-3.5.6.tar.gz
-tar -xvf apache-zookeeper-3.5.6.tar
-mv /installer/apache-zookeeper-3.5.6 /opt/apache-zookeeper-3.5.6
-
-cp /opt/apache-zookeeper-3.5.6/conf/zoo_sample.cfg /opt/apache-zookeeper-3.5.6/conf/zoo.cfg
-
-cat >/opt/apache-zookeeper-3.5.6/conf/zoo.cfg <<EOL
-tickTime=2000
-dataDir=/var/zookeeper
-clientPort=2181
-EOL
-
-cd /opt/apache-zookeeper-3.5.6
-bin/zkServer.sh start /opt/apache-zookeeper-3.5.6/conf/zoo.cfg
+sudo su
+#########################
+#   install JDK
+#########################
+#https://docs.aws.amazon.com/corretto/latest/corretto-8-ug/amazon-linux-install.html
+amazon-linux-extras enable corretto8
+yum install -y java-1.8.0-amazon-corretto
+yum install -y java-1.8.0-amazon-corretto-devel
+alternatives --config javac
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-amazon-corretto.x86_64
 
 # this will install kafka on EC2 and running in single instance mode for testing purpose
 #https://kafka.apache.org/
-
+#########################
+#   install kafka
+#########################
 mkdir -p /installer
 cd /installer
 wget http://apache.mirror.amaze.com.au/kafka/2.3.0/kafka_2.12-2.3.0.tgz
 tar -xzf kafka_2.12-2.3.0.tgz
 mv /installer/kafka_2.12-2.3.0 /opt/kafka_2.12-2.3.0
+cd /opt/kafka_2.12-2.3.0
+bin/zookeeper-server-start.sh config/zookeeper.properties &
+bin/kafka-server-start.sh config/server.properties &
+
+
+
+#########################
+#   Create Topics
+#########################
+bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic test
+
+
+bin/kafka-console-consumer.sh \
+    --bootstrap-server localhost:9092 \
+    --topic test \
+    --from-beginning
+
+bin/kafka-topics.sh --create \
+      --zookeeper localhost:2181 \
+      --replication-factor 1 --partitions 1 \
+      --topic test
